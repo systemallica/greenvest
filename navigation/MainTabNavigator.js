50aa2f031c7062022fd3217409e00fc7f05d0a0d
Andrés Reverón Molina <andressser@gmail.com>
2019-10-25 16:44:01 +0200

    add map component

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -26,8 +26,8 @@ HomeStack.navigationOptions = {
       focused={focused}
       name={
         Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
+          ? `ios-map${focused ? '' : '-outline'}`
+          : 'md-map'
       }
     />
   ),
