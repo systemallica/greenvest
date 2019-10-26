60c3040632f7a88742dd15b4293428b13b18e544
Andrés Reverón Molina <andressser@gmail.com>
2019-10-26 09:46:42 +0200

    pie chart

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -2,52 +2,13 @@ import React from "react";
 import { Dimensions, View } from "react-native";
 import { LineChart, PieChart } from "react-native-chart-kit";
 import { Text, Body, Container } from "native-base";
+import purchases from "../stub/purchases";
+
 export default function DashboardScreen() {
-  state = {};
-
-  const data = [
-    {
-      name: "Seoul",
-      population: 21500000,
-      color: "rgba(131, 167, 234, 1)",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15
-    },
-    {
-      name: "Toronto",
-      population: 2800000,
-      color: "#F00",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15
-    },
-    {
-      name: "Beijing",
-      population: 527612,
-      color: "red",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15
-    },
-    {
-      name: "New York",
-      population: 8538000,
-      color: "#ffff00",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15
-    },
-    {
-      name: "Moscow",
-      population: 11920000,
-      color: "rgb(0, 0, 255)",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15
-    }
-  ];
-
   const chartConfig = {
     backgroundColor: "#e26a00",
     backgroundGradientFrom: "#fb8c00",
     backgroundGradientTo: "#ffa726",
-    decimalPlaces: 2, // optional, defaults to 2dp
     color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
     labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
     style: {
@@ -59,6 +20,38 @@ export default function DashboardScreen() {
       stroke: "#ffa726"
     }
   };
+
+  const aggregatedPurchaseCategories = purchases.reduce((results, purchase) => {
+    results.categories.green += purchase.categories.green;
+    results.categories.red += purchase.categories.red;
+    results.categories.yellow += purchase.categories.yellow;
+
+    return results;
+  });
+
+  const purchaseCategoriesParsedForPiechart = [
+    {
+      name: "Green",
+      value: aggregatedPurchaseCategories.categories.green,
+      color: "green",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Red",
+      value: aggregatedPurchaseCategories.categories.red,
+      color: "red",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Yellow",
+      value: aggregatedPurchaseCategories.categories.yellow,
+      color: "yellow",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    }
+  ];
 
   return (
     <Container>
@@ -93,16 +86,17 @@ export default function DashboardScreen() {
           }}
         />
       </View>
+
       <View>
         <Body>
           <Text>Pie Chart</Text>
         </Body>
         <PieChart
-          data={data}
+          data={purchaseCategoriesParsedForPiechart}
           width={Dimensions.get("window").width}
           height={220}
           chartConfig={chartConfig}
-          accessor="population"
+          accessor="value"
           backgroundColor="transparent"
           paddingLeft="15"
           absolute
