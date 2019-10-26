e340a0d1da3e6248cfccdb2cc53d8eda2e3ef87a
ferranconde <fcondecodorniu@gmail.com>
2019-10-26 09:10:29 +0200

    Scan details page

--- components/ScanDetails.js
+++ components/ScanDetails.js
@@ -7,7 +7,29 @@ export default class ScanDetails extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { ...props.scanProps };
+        const purchase = this.parsePurchase(props.scanProps.data);
+        const details = this.getDetails(purchase);
+        this.state = {
+            purchase,
+            details,
+        };
+    }
+
+    parsePurchase(jsonString) {
+        if (jsonString !== undefined) {
+            try {
+                const details = JSON.parse(jsonString);
+                return details;
+            } catch (err) {
+                return {};
+            }
+        } else {
+            return {};
+        }
+    }
+
+    parseDetails(purchase) {
+        return undefined;
     }
 
 
