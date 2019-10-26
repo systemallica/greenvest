9ef6795b456c664f0d6d75f12d97cfa459958723
Andrés Reverón Molina <andressser@gmail.com>
2019-10-26 08:41:46 +0200

    create mock data

--- screens/DiscoverScreen.js
+++ screens/DiscoverScreen.js
@@ -12,57 +12,15 @@ import {
   Right
 } from "native-base";
 
+import discounts from "../stub/discounts"
+
 export default function DiscoverScreen() {
-  state = {
-    discounts: [
-      {
-        uri:
-          "https://tce-live2.s3.amazonaws.com/media/media/edce9657-3d08-4b7c-b369-aa52cc7f8d79.jpg",
-        title: "Tomatoes",
-        description: "Fresh tomatoes",
-        distance: "0.98km",
-        old_price: "40",
-        new_price: "20",
-        shop: "Old Joe's"
-      },
-      {
-        uri:
-          "https://images.newscientist.com/wp-content/uploads/2019/01/31134057/gettyimages-86304874.jpg",
-        title: "Bananas",
-        description: "Fresh bananas",
-        distance: "2.3km",
-        old_price: "60",
-        new_price: "10",
-        shop: "Hanna's fruit shop"
-      },
-      {
-        uri:
-          "http://2.bp.blogspot.com/-zrL0VD4W6og/UPham_76fZI/AAAAAAAAALg/IekKGx0vx6Q/s1600/Curly+Kale.JPG",
-        title: "Kale",
-        description: "Fresh kale",
-        distance: "0.08km",
-        old_price: "50",
-        new_price: "20",
-        shop: "Carrefour"
-      },
-      {
-        uri:
-          "http://www.theluxuryspot.com/wp-content/uploads/2014/11/Soy_milk.jpg",
-        title: "Soy milk",
-        description: "Soy milk",
-        distance: "1.8km",
-        old_price: "23",
-        new_price: "20",
-        shop: "Colruyt"
-      }
-    ]
-  };
 
   return (
     <Container style={styles.container}>
       <Content>
         <List>
-          {this.state.discounts.map((discount, i) => (
+          {discounts.map((discount, i) => (
             <ListItem thumbnail key={i}>
               <Left>
                 <Thumbnail
