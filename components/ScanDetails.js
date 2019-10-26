5b0f346ba0eb331bed67f7e194336e475195d971
Ferran Conde Codorniu <fcondecodorniu@gmail.com>
2019-10-26 14:03:52 +0200

    QR scan, calculate params, show summary (#13)
    * wip
    
    * Scan barcode/QR screen
    
    * Jump to another page
    
    * Scan details page
    
    * TODO Calculate
    
    * Formato
    
    * wip, qr
    
    * working
    
    * margin
    
    * Computation of ecopoints added.
    
    * Working
    
    * Las cosas no peta

--- components/ScanDetails.js
+++ components/ScanDetails.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import translator from "../stub/translator";
 import {
     View,
     Text,
@@ -12,13 +13,13 @@ import {
     Card,
     CardItem,
     Thumbnail,
+    Button,
+    Icon,
 } from 'native-base';
 
 import { PieChart } from "react-native-chart-kit";
 
 export default class ScanDetails extends React.Component {
-  
-
 
     constructor(props) {
         super(props);
@@ -35,61 +36,127 @@ export default class ScanDetails extends React.Component {
             }
         }
 
-        if (purchase === undefined) {
-            // calculate score
-            details = {
-                greenScore: 70,
-                amount: 50.16,
-                telenetScore: 7,
-                categories: {
-                    green: 10,
-                    yellow: 2,
-                    red: 10,
+        if (purchase !== undefined) {
+            
+            score = {};
+            console.log(purchase);
+            purchase_weight = purchase.items.length;
+            console.log("purchase weight ",purchase_weight);
+            // Products from abroad are supposed to have gone from Panama to Belgium (8808 km) which translates to 0.89 Kg of carbon footprint per product. 
+            footprint_normalizer = Math.max.apply(Math, translator.category_translation.map(function(o) { return o.weight; }));
+            // Packaging supposed to be 5 per cent
+            footprint_normalizer += 0.05*Math.max.apply(Math, translator.packaging_translation.map(function(o) { return o.weight; }));
+            footprint_normalizer += 0.89;
+            console.log("normalizer ",footprint_normalizer);
+            scores =[];
+            total_score = 0;
+
+            purchase.items.forEach(item => {
+                prod_fp_w = translator.category_translation.find(categ => categ.category == item["category"] ).weight;
+                ct_fp_w = translator.country_translation.find(categ => categ.category == item["country"]);
+                if (ct_fp_w !== undefined) {
+                    ct_fp_w = ct_fp_w.weight;
+                } else {
+                    ct_fp_w = 0.89;
                 }
+                pkg_fp_w = translator.packaging_translation.find(categ => categ.category == item["packaging"]).weight;
+                item_score = (prod_fp_w + ct_fp_w + pkg_fp_w*0.05 )/(footprint_normalizer);
+                console.log("item score ",item_score);
+                total_score += item.weight*(prod_fp_w + ct_fp_w + pkg_fp_w*0.05 )/(purchase_weight*footprint_normalizer);
+                console.log("total score ",total_score);
+                scores.push(
+                    {
+                        "item": item.category,
+                        "score": item_score
+                    }
+                );                
+            });
+
+            //If carbon footprint is less than 2kg is considered a green product
+
+            let green_amount = 0;
+            let yellow_amount = 0;
+            let red_amount = 0;
+
+            scores.forEach(item => {
+                if (item.score < 2.0) {
+                    green_amount++;
+                } else if (item.score < 7.0) {
+                    yellow_amount++;
+                } else {
+                    red_amount++;
+                }
+            });
+
+            const greenScore = (1 - total_score);
+            // Telenet score is the ecopoints multiplied by the amount of money you have spent
+            const telenetScore = purchase.total * greenScore;
+
+            details = {
+                greenScore: (greenScore * 100).toFixed(2),
+                amount: purchase.total.toFixed(2),
+                telenetScore: telenetScore.toFixed(2),
+                categories: {
+                    green: green_amount,
+                    yellow: yellow_amount,
+                    red: red_amount,
+                },
             };
+
+            console.log(details);
         }
 
-        const pieChartData = [
-            {
-                name: "  < 2kg",
-                amount: details.categories.green,
-                color: '#00FF00',
-                legendFontColor: "#000000",
-                legendFontSize: 15
-            },
-            {
-                name: "  2 - 7 kg",
-                amount: details.categories.yellow,
-                color: '#FFFF00',
-                legendFontColor: "#000000",
-                legendFontSize: 15
-            },
-            {
-                name: "  > 7kg",
-                amount: details.categories.red,
-                color: '#FF0000',
-                legendFontColor: "#000000",
-                legendFontSize: 15
-            },
+        if (details !== undefined) {
+            const pieChartData = [
+                {
+                    name: "  < 2kg",
+                    amount: details.categories.green,
+                    color: '#00FF00',
+                    legendFontColor: "#000000",
+                    legendFontSize: 15
+                },
+                {
+                    name: "  2 - 7 kg",
+                    amount: details.categories.yellow,
+                    color: '#FFFF00',
+                    legendFontColor: "#000000",
+                    legendFontSize: 15
+                },
+                {
+                    name: "  > 7kg",
+                    amount: details.categories.red,
+                    color: '#FF0000',
+                    legendFontColor: "#000000",
+                    legendFontSize: 15
+                },
+    
+            ];
+    
+            const chartConfig = {
+                backgroundGradientFrom: 'transparent',
+                backgroundGradientFromOpacity: 0,
+                backgroundGradientTo: 'transparent',
+                backgroundGradientToOpacity: 0.5,
+                color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+                strokeWidth: 2, // optional, default 3
+                barPercentage:0.5
+              }
+    
+            this.state = {
+                purchase,
+                details,
+                chartConfig,
+                pieChartData,
+            };
+        } else {
+            this.state = { error: true };
+        }
 
-        ];
+        
+    }
 
-        const chartConfig = {
-            backgroundGradientFrom: 'transparent',
-            backgroundGradientFromOpacity: 0,
-            backgroundGradientTo: 'transparent',
-            backgroundGradientToOpacity: 0.5,
-            color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-            strokeWidth: 2, // optional, default 3
-            barPercentage:0.5
-          }
-
-        this.state = {
-            purchase,
-            details,
-            chartConfig,
-            pieChartData,
-        };
+    handleClose() {
+        this.props.closeCallback();
     }
 
 
@@ -97,26 +164,44 @@ export default class ScanDetails extends React.Component {
         return (
             <Container style={this.styles.container}>
                 <Content>
+                    <View style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'flex-end',
+                        width: Dimensions.get('window').width - 20,
+                    }}>
+                        <Button iconLeft transparent onPress={ this.handleClose.bind(this) }>
+                            <Icon name='close' />
+                        </Button>
+                    </View>
+                    <Text style={{
+                        textAlign: 'center',
+                        fontWeight: 'bold',
+                        fontSize: 21,
+                        marginTop: '5%',
+                        marginBottom: '5%',
+                    }}>
+                        Purchase summary
+                    </Text>
                     <Card>
                         <CardItem style={this.styles.cardItem}>
                             <View style={this.styles.cardItemBadge}>
-                                <Thumbnail square source={require("../assets/images/icon.png")} />
-                                <Text style={{ marginTop: 5 }}>{this.state.details.greenScore}</Text>
+                                <Thumbnail large square source={require("../assets/images/icon.png")} />
+                                <Text style={{ marginTop: 5 }}>{this.state.details.greenScore}%</Text>
                             </View>
                             <View style={this.styles.cardItemBadge}>
-                                <Thumbnail square source={require("../assets/images/euro.png")} />
-                                <Text style={{ marginTop: 5 }}>{this.state.details.amount}</Text>
+                                <Thumbnail large square source={require("../assets/images/euro.png")} />
+                                <Text style={{ marginTop: 5 }}>{this.state.details.amount}€</Text>
                             </View>
                             <View style={this.styles.cardItemBadge}>
                                 <Thumbnail
                                     square
+                                    large
                                     source={require("../assets/images/telenet.png")}
                                 />
                                 <Text style={{ marginTop: 5 }}>{this.state.details.telenetScore}</Text>
                             </View>
                         </CardItem>
-                    </Card>
-                    <Card>
                         <CardItem style={{display: 'flex', justifyContent: 'center'}}>
                             <PieChart
                                 data={this.state.pieChartData}
@@ -125,8 +210,9 @@ export default class ScanDetails extends React.Component {
                                 accessor="amount"
                                 backgroundColor="transparent"
                                 chartConfig={this.state.chartConfig}
+                                absolute
                             />
-                        </CardItem>    
+                        </CardItem>
                     </Card>
                 </Content>
             </Container>
---
+++ stub/translator.js
@@ -0,0 +1,96 @@
+export default translator = {
+    category_translation: [
+        {
+            category: "lamb",
+            weight: 39.2
+        },
+        {
+            category: "beef",
+            weight: 27.0
+        },
+        {
+            category: "cheese",
+            weight: 13.5
+        },
+        {
+            category: "pork",
+            weight: 12.1
+        },
+        {
+            category: "turkey",
+            weight: 10.9
+        },
+        {
+            category: "chiken",
+            weight: 6.9
+        },
+        {
+            category: "tuna",
+            weight: 6.1
+        },
+        {
+            category: "eggs",
+            weight: 4.8
+        },
+        {
+            category: "potatoes",
+            weight: 2.9
+        },
+        {
+            category: "rice",
+            weight: 2.7
+        },
+        {
+            category: "nuts",
+            weight: 2.3
+        },
+        {
+            category: "beans",
+            weight: 2.0
+        },
+        {
+            category: "vegetables",
+            weight: 2.0
+        },
+        {
+            category: "milk",
+            weight: 1.9
+        },
+        {
+            category: "fruits",
+            weight: 1.1
+        },        {
+            category: "lentils",
+            weight: 1.1
+        }
+    ],
+    packaging_translation: [
+        {
+            category: "plastic",
+            weight: 6.0
+        },
+        {
+            category: "aluminium",
+            weight: 6.0
+        },
+        {
+            category: "wood",
+            weight: 6.0
+        },
+        {
+            category: "cardboard",
+            weight: 6.0
+        },
+        {
+            category: "glass",
+            weight: 6.0
+        },
+    ]
+    ,country_translation: [
+        {
+            category: "BE",
+            weight: 0.0
+        },
+    ]
+
+};
